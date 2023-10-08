0027c49b0089429cc928606c8aa5324f5697eec8
dmitry <d.shiriaev@bars.group>
2023-10-08 15:03:55 +0400

    *

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,9 @@
 <template>
-    <div>123123123</div>
+    <ul>
+        <li><router-link to="/route">маршруты</router-link></li>
+        <li><router-link to="/locationCard">локация</router-link></li>
+        <li><router-link to="/routePoint">маршрут</router-link></li>
+    </ul>
 </template>
 
 <script setup lang="ts">
