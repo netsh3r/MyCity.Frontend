7a0a269b3d1a189301b52febc7b0fb156c94c734
dmitry <d.shiriaev@bars.group>
2023-10-08 14:08:20 +0400

    Update RoutePointView.vue

--- src/views/RoutePointView.vue
+++ src/views/RoutePointView.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import axios from 'axios';
 import draggable from "vuedraggable"
-const dragging = ref(true);
 const list = ref([
     { id: 1, name: 1, description: "123" },
     { id: 1, name: 1, description: "123" },
@@ -20,7 +18,7 @@ const list = ref([
                     <v-combobox class="col-10" placeholder="локация" bg-color="white"></v-combobox>
                     <v-btn height="55" class="col-2">добавить</v-btn>
                 </div>
-                <draggable v-model="list" item-key="id" @start="drag = true" @end="drag = false" class="list-group"
+                <draggable v-model="list" item-key="id" class="list-group"
                     ghost-class="ghost">
                     <template #item="{ element }">
                         <v-card :title="element.name" class="mt-3" :text="element.description">
